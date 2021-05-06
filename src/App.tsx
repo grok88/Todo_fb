2c26ecc054ff894e974084a9211b036daee2e7de
Alex <grok88@tut.by>
2021-05-06 11:15:58 +0300

    add useEffect - read data from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {db} from './firebase';
 
-function App() {
+const App = () => {
+    console.log('App');
+    const [todos, setTodos] = useState<Array<{ title: string, id: string }>>([]);
+
+    useEffect(() => {
+        db.collection('todos')
+            // .where('capital', '==', true)
+            .get()
+            .then((snapshot) => {
+                const todoData = snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+                console.log(todoData)
+                // @ts-ignore
+                setTodos(todoData);
+
+                // snapshot.forEach((doc) => {
+                //     // doc.data() is never undefined for query doc snapshots
+                //     console.log(doc.id, ' => ', doc.data());
+                // });
+            })
+            .catch((error) => {
+                console.log('Error getting documents: ', error);
+            });
+    }, []);
 
     return (
         <div className="App">
-            APP
+            <ul>
+                {
+                    todos.map(todo => <li key={todo.id}>{todo.title}</li>)
+                }
+            </ul>
         </div>
     );
 }
